a5b7bfc87ebd8d7da3785b58473288c629f3dd22
tubi55 <tubi55@nate.com>
2024-06-01 21:41:22 +0900

    팀 소개 섹션 반응형 스타일링

--- src/assets/data.js
+++ src/assets/data.js
@@ -29,4 +29,13 @@ const quickNavData = [
 	}
 ];
 
-export { quickNavData };
+const memberInfo = [
+	{ name: "Paul Yoon", pic: "paul.jpg", position: "CFO" },
+	{ name: "Emily Chung", pic: "emily.jpg", position: "CTO" },
+	{ name: "Peter Smith", pic: "peter.jpg", position: "UI Designer" },
+	{ name: "Julia Min", pic: "julia.jpg", position: "Frontend Dev" },
+	{ name: "Michael Hall", pic: "michael.jpg", position: "Backend Dev" },
+	{ name: "Emma Smith", pic: "emma.jpg", position: "Project Manager" }
+];
+
+export { quickNavData, memberInfo };
--- src/pages/Members.jsx
+++ src/pages/Members.jsx
@@ -3,6 +3,7 @@ import Intro from "../components/Intro";
 import Layout from "../components/Layout";
 import Thumbnail from "../components/Thumbnail";
 import Line from "../components/Line";
+import { memberInfo } from "../assets/data";
 
 function Members() {
 	return (
@@ -24,7 +25,7 @@ function Members() {
 				<article className="flex flex-wrap justify-between">
 					{/* CEO info */}
 					<div className="w-[60%] text-right pr-20 pt-40 max_lg:pt-0 max_sm:w-full max_sm:text-left max_sm:pr-0">
-						<h3 className="pr-1 text-lg opacity-70">CEO</h3>
+						<h3 className="pr-1 text-lg opacity-70 tracking-widest">CEO</h3>
 
 						<h4 className="text-4xl">DAVID, CHOI</h4>
 
@@ -44,6 +45,52 @@ function Members() {
 					{/* CEO pic */}
 					<div className="w-[40%] h-[40vmax] -translate-y-[20%] max_lg:translate-y-[0%] max_sm:w-full max_sm:h-[60vw]">
 						<Thumbnail className="size-full" src={"/david.jpg"} />
+					</div>
+				</article>
+
+				{/* Team */}
+				<article className="flex flex-wrap justify-between mt-36">
+					{/* title & intro */}
+					<h3 className="w-full mb-4 text-4xl font-bold font-raleway">
+						Our Team Members
+					</h3>
+					<p className="text-lg opacity-50 mb-14">
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam
+						provident nulla quod expedita ipsum fugit unde sapiente facilis
+						quidem nihil. Modi sed quae quia repudiandae. Atque necessitatibus
+						fugiat cupiditate magni?
+					</p>
+
+					{/* team list */}
+					<ul className="w-[60%] flex flex-wrap justify-between mt-10 max_lg:w-full">
+						{memberInfo.map(({ name, pic, position }, idx) => (
+							<li key={idx} className="w-[28%] h-[15vmax] mb-40 max_sm:w-[45%]">
+								<Thumbnail src={"/" + pic} className="opacity-80 " />
+								<div className="relative mt-6">
+									<h2 className="text-xl font-semibold">{name}</h2>
+									<p className="text-sm mb-7 text-black/60 tracking-wide">
+										{position}
+									</p>
+								</div>
+							</li>
+						))}
+					</ul>
+
+					{/* team content */}
+					<div className="w-[26%] content-center max_lg:w-full max_lg:mb-20">
+						<h3 className="mb-4 text-xl font-bold font-raleway">
+							Lorem ipsum dolor sit.
+						</h3>
+						<Line className="mb-8 w-[20%] h-[2px]" delay={3} />
+						<p className="text-sm text-justify opacity-70">
+							Lorem, ipsum dolor sit amet cons ectetur adipisi cing elit. Esse,
+							arch itecto fyhsrg.
+							<br />
+							<br /> Quisq uam labor iosam offi ciis amet tem por ibus alias mol
+							litia quas volup tate, animi error re pudi andae fuga impe dit
+							dele niti, ullam ad ipisci ipsa volu ptate, animi pudi andae fuga
+							impe dit dele niti, ullam ad ipisci ipsa volu ptate, animi
+						</p>
 					</div>
 				</article>
 			</Content>
