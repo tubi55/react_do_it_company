89b6267de568aa4d63efbaf0868779556f989061
tubi55 <tubi55@nate.com>
2024-06-12 13:38:56 +0900

    VITE에서 환경변수 등록-리액트환경변수 등록과 다름 주의

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -13,7 +13,7 @@ function Gallery() {
 	useEffect(() => {
 		const fetchFlickr = async () => {
 			const baseURL = "https://www.flickr.com/services/rest/?format=json&nojsoncallback=1";
-			const key = "2a1a0aebb34012a99c23e13b49175343";
+			const key = import.meta.env.VITE_FLICKR_KEY;
 			const method_interest = "flickr.interestingness.getList";
 			const num = 40;
 			const url = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
