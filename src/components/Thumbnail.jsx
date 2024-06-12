c5ff247ce7a37a46fb795e9b25255def1a24b428
tubi55 <tubi55@nate.com>
2024-06-12 13:31:13 +0900

    h_auto일때 position도 relative로 변경

--- src/components/Thumbnail.jsx
+++ src/components/Thumbnail.jsx
@@ -9,7 +9,7 @@ import { twMerge } from "tailwind-merge";
 function Thumbnail({ src = "", text = "alt text", shadow = true, className, h_auto = false }) {
 	return (
 		<figure className={twMerge("size-full transition-all relative", className)}>
-			{shadow && <img className={twMerge("translate-x-2 translate-y-2 pic blur-md z-400", h_auto && "!h-auto")} src={src} alt={text} />}
+			{shadow && <img className={twMerge("translate-x-2 translate-y-2 pic blur-md z-400", h_auto && "!h-auto !relative")} src={src} alt={text} />}
 			<img className={twMerge("pic", h_auto && "!h-auto")} src={src} alt={text} />
 		</figure>
 	);
