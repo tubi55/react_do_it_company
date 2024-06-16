7a05a577fb984c5bbfe2cdc62ffe52793313e251
tubi55 <tubi55@nate.com>
2024-06-16 14:51:43 +0900

    모달창 기능 추가 및 갤러리 버튼 클릭시 마이 갤러리 나오는 오류 fetching함수에 적용된 useCallback제거해서 해결

--- src/components/Thumbnail.jsx
+++ src/components/Thumbnail.jsx
@@ -1,14 +1,8 @@
-//npm i tailwind-merge
-//쓰는 이유, 부모의 props로 className을 전달받아 합치는 경우 기존엔 문제발생
-//뒤에 붙은 추후에 붙은 className을 덮어쓰는 것이 아닌 이미 기존의 적용된 스타일을 먼저 처리하기 때문
-//twMerge(기존 클래스, 덮어쓸 클래스)
 import { twMerge } from "tailwind-merge";
 
-// h_auto값이 true로 들어오면 자식 이미지요소의 원래 높이값을 활용 (부모높이의 100%사용하지 않음)
-// masonry ui연결할 때 필요
-function Thumbnail({ src = "", text = "alt text", shadow = true, className, h_auto = false }) {
+function Thumbnail({ src = "", text = "alt text", shadow = true, className, h_auto = false, onClick = null }) {
 	return (
-		<figure className={twMerge("size-full transition-all relative", className)}>
+		<figure className={twMerge("size-full transition-all relative", className)} onClick={onClick}>
 			{shadow && <img className={twMerge("translate-x-2 translate-y-2 pic blur-md z-400", h_auto && "!h-auto !relative")} src={src} alt={text} />}
 			<img className={twMerge("pic", h_auto && "!h-auto")} src={src} alt={text} />
 		</figure>
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -7,6 +7,7 @@ import { motion } from "framer-motion";
 import Thumbnail from "../components/Thumbnail";
 import Masonry from "react-masonry-component";
 import { LuSearch } from "react-icons/lu";
+import Modal from "../components/Modal";
 
 function Gallery() {
 	const delay = 1.4;
@@ -14,41 +15,40 @@ function Gallery() {
 	const [Pics, setPics] = useState([]);
 	let [IsUser, setIsUser] = useState(myID);
 	let [CurrentType, setCurrentType] = useState("mine");
+	let [IsOpen, setIsOpen] = useState(false);
+	const [Index, setIndex] = useState(0);
 	const ref_btnSet = useRef(null);
 	const ref_frame = useRef(null);
 	const ref_input = useRef(null);
 
-	const fetchFlickr = useCallback(
-		async opt => {
-			const baseURL = "https://www.flickr.com/services/rest/?format=json&nojsoncallback=1";
-			const key = import.meta.env.VITE_FLICKR_KEY;
-			const num = 40;
-			let url = "";
+	const fetchFlickr = async opt => {
+		const baseURL = "https://www.flickr.com/services/rest/?format=json&nojsoncallback=1";
+		const key = import.meta.env.VITE_FLICKR_KEY;
+		const num = 40;
+		let url = "";
 
-			const method_user = "flickr.people.getPhotos";
-			const method_interest = "flickr.interestingness.getList";
-			const method_search = "flickr.photos.search";
+		const method_user = "flickr.people.getPhotos";
+		const method_interest = "flickr.interestingness.getList";
+		const method_search = "flickr.photos.search";
 
-			const url_interest = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
-			const url_user = `${baseURL}&api_key=${key}&method=${method_user}&per_page=${num}&user_id=${opt.id}`;
-			const url_search = `${baseURL}&api_key=${key}&method=${method_search}&per_page=${num}&tags=${opt.keyword}`;
+		const url_interest = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
+		const url_user = `${baseURL}&api_key=${key}&method=${method_user}&per_page=${num}&user_id=${opt.id}`;
+		const url_search = `${baseURL}&api_key=${key}&method=${method_search}&per_page=${num}&tags=${opt.keyword}`;
 
-			opt.type === "user" && (url = url_user);
-			opt.type === "interest" && (url = url_interest);
-			opt.type === "search" && (url = url_search);
+		opt.type === "user" && (url = url_user);
+		opt.type === "interest" && (url = url_interest);
+		opt.type === "search" && (url = url_search);
 
-			const data = await fetch(url);
-			const json = await data.json();
-			if (json.photos.photo.length === 0) {
-				const [btnInterest, btnMine] = ref_btnSet.current.querySelectorAll("button");
-				CurrentType === "interest" && btnInterest.classList.add("on");
-				CurrentType === "mine" && btnMine.classList.add("on");
-				return alert("해당 검색어의 결과값이 없습니다.");
-			}
-			setPics(json.photos.photo);
-		},
-		[CurrentType]
-	);
+		const data = await fetch(url);
+		const json = await data.json();
+		if (json.photos.photo.length === 0) {
+			const [btnMine, btnInterest] = ref_btnSet.current.querySelectorAll("button");
+			CurrentType === "interest" && btnInterest.classList.add("on");
+			CurrentType === "mine" && btnMine.classList.add("on");
+			return alert("해당 검색어의 결과값이 없습니다.");
+		}
+		setPics(json.photos.photo);
+	};
 
 	const activateBtn = e => {
 		window.scrollTo({ top: 0, behavior: "smooth" });
@@ -100,100 +100,120 @@ function Gallery() {
 		setCurrentType("search");
 	};
 
+	const handleModal = idx => {
+		console.log("handleModal", idx);
+		//Modal안의 컨텐츠를 출력하기 위한 State
+		setIsOpen(true);
+		//클릭한 썸네일의 순번값을 전달하기 위한 State
+		setIndex(idx);
+	};
+
 	useEffect(() => {
 		fetchFlickr({ type: "user", id: myID });
 		setTimeout(() => {
 			ref_frame.current.classList.add("on");
 		}, 2000);
-	}, [fetchFlickr]);
+	}, []);
 
 	return (
-		<Layout title={"GALLERY"}>
-			<Intro>
-				<div className="mframe">
-					<motion.span
-						initial={{ opacity: 0 }}
-						animate={{ opacity: 1 }}
-						exit={{ opacity: 0, transition: { delay: 0 } }}
-						transition={{ duration: 0.05, delay: delay + 0.25 }}>
-						Dolor sit lorem ipsum dolor sit amet Aspe riores ipsum dolor sit amet consec, beatae ipsum dolor.
-					</motion.span>
-					<Mask delay={delay} />
-				</div>
-				<br />
-				<div className="mframe">
-					<motion.span
-						initial={{ opacity: 0 }}
-						animate={{ opacity: 1 }}
-						exit={{ opacity: 0, transition: { delay: 0 } }}
-						transition={{ duration: 0.05, delay: delay + 0.25 + 0.2 }}>
-						Bit ametDolor sit amet consectetur adipisicing elit ctetur adipisi.
-					</motion.span>
-					<Mask delay={delay + 0.2} />
-				</div>
-			</Intro>
+		<>
+			<Layout title={"GALLERY"}>
+				<Intro>
+					<div className="mframe">
+						<motion.span
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							exit={{ opacity: 0, transition: { delay: 0 } }}
+							transition={{ duration: 0.05, delay: delay + 0.25 }}>
+							Dolor sit lorem ipsum dolor sit amet Aspe riores ipsum dolor sit amet consec, beatae ipsum dolor.
+						</motion.span>
+						<Mask delay={delay} />
+					</div>
+					<br />
+					<div className="mframe">
+						<motion.span
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							exit={{ opacity: 0, transition: { delay: 0 } }}
+							transition={{ duration: 0.05, delay: delay + 0.25 + 0.2 }}>
+							Bit ametDolor sit amet consectetur adipisicing elit ctetur adipisi.
+						</motion.span>
+						<Mask delay={delay + 0.2} />
+					</div>
+				</Intro>
 
-			<Content>
-				{/* control bar */}
-				<article className="flex flex-wrap justify-between px-5 py-4 mb-10">
-					{/* gallery type button */}
-					<nav className="w-[40%] flex gap-7" ref={ref_btnSet}>
-						<button className="btn_line on" onClick={handleMine}>
-							My Gallery
-						</button>
-						<button className="btn_line" onClick={handleInterest}>
-							Interest Gallery
-						</button>
-					</nav>
+				<Content>
+					{/* control bar */}
+					<article className="flex flex-wrap justify-between px-5 py-4 mb-10">
+						{/* gallery type button */}
+						<nav className="w-[40%] flex gap-7" ref={ref_btnSet}>
+							<button className="btn_line on" onClick={handleMine}>
+								My Gallery
+							</button>
+							<button className="btn_line" onClick={handleInterest}>
+								Interest Gallery
+							</button>
+						</nav>
 
-					{/* serach form */}
-					<form onSubmit={handleSubmit} className="relative flex items-center gap-2">
-						<input
-							type="text"
-							placeholder="Search Keyword"
-							ref={ref_input}
-							className="bg-transparent w-[200px] border-b border-black/50 px-2 py-2 pr-6 outline-none"
-						/>
-						<button className="absolute right-0 bg-transparent border-none cursor-pointer bottom-4">
-							<LuSearch fontSize={20} color={"#555"} />
-						</button>
-					</form>
-				</article>
+						{/* serach form */}
+						<form onSubmit={handleSubmit} className="relative flex items-center gap-2">
+							<input
+								type="text"
+								placeholder="Search Keyword"
+								ref={ref_input}
+								className="bg-transparent w-[200px] border-b border-black/50 px-2 py-2 pr-6 outline-none"
+							/>
+							<button className="absolute right-0 bg-transparent border-none cursor-pointer bottom-4">
+								<LuSearch fontSize={20} color={"#555"} />
+							</button>
+						</form>
+					</article>
 
-				{/* gallery list frame */}
-				<div ref={ref_frame} className="w-full transition opacity-0 translate-y-24 [&.on]:opacity-100 [&.on]:translate-y-0">
-					{/* Masonry */}
-					<Masonry elementType={"div"} spacing={2} options={{ transitionDuration: "0.5s" }} disableImagesLoaded={false} updateOnEachImageLoad={false}>
-						{Pics.map((pic, idx) => {
-							return (
-								<article key={idx} className="w-[25%] mb-14  max_2xl:w-[33.333%] max_lg:w-[50%] max_sm:w-[100%] px-5">
-									<Thumbnail
-										src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
-										className="w-full mb-1 [&_img:first-child]:opacity-70"
-										h_auto={true}
-									/>
-
-									<h2 className="my-5 font-lg">{pic.title}</h2>
-
-									{/* profile Box */}
-									<div className="flex items-end w-full gap-3 pb-3 border-b border-black/40">
-										<img
-											className="w-10"
-											src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
-											alt={pic.owner}
-											onError={e => e.target.setAttribute("src", "https://www.flickr.com/images/buddyicon.gif")}
+					{/* gallery list frame */}
+					<div ref={ref_frame} className="w-full transition opacity-0 translate-y-24 [&.on]:opacity-100 [&.on]:translate-y-0">
+						{/* Masonry */}
+						<Masonry
+							elementType={"div"}
+							spacing={2}
+							options={{ transitionDuration: "0.5s" }}
+							disableImagesLoaded={false}
+							updateOnEachImageLoad={false}>
+							{Pics.map((pic, idx) => {
+								return (
+									<article key={idx} className="w-[25%] mb-14  max_2xl:w-[33.333%] max_lg:w-[50%] max_sm:w-[100%] px-5 cursor-pointer">
+										<Thumbnail
+											src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
+											className="w-full mb-1 [&_img:first-child]:opacity-70"
+											h_auto={true}
+											onClick={() => handleModal(idx)}
 										/>
-										<span className="transition cursor-pointer hover:text-sky-600" onClick={handleUser}>
-											{pic.owner}
-										</span>
-									</div>
-								</article>
-							);
-						})}
-					</Masonry>
-				</div>
-			</Content>
-		</Layout>
+
+										<h2 className="my-5 font-lg">{pic.title}</h2>
+
+										{/* profile Box */}
+										<div className="flex items-end w-full gap-3 pb-3 border-b border-black/40">
+											<img
+												className="w-10"
+												src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
+												alt={pic.owner}
+												onError={e => e.target.setAttribute("src", "https://www.flickr.com/images/buddyicon.gif")}
+											/>
+											<span className="transition cursor-pointer hover:text-sky-600" onClick={handleUser}>
+												{pic.owner}
+											</span>
+										</div>
+									</article>
+								);
+							})}
+						</Masonry>
+					</div>
+				</Content>
+			</Layout>
+
+			<Modal IsOpen={IsOpen} setIsOpen={setIsOpen}>
+				{Pics[Index] && <img src={`https://live.staticflickr.com/${Pics[Index].server}/${Pics[Index].id}_${Pics[Index].secret}_b.jpg`} alt="pic" />}
+			</Modal>
+		</>
 	);
 }
 
